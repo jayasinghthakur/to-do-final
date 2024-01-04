7d4805aae4d1ddf2bc2490562a606ffe833c7b51
Jaya Singh <137374743+jayasinghthakur@users.noreply.github.com>
2024-01-05 01:51:24 +0530

    Update app.js

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 5000;
 
 const app = express();
 
-mongoose.connect("mongodb://127.0.0.1:27017/toDoListDB");
+mongoose.connect(process.env.MONGO_URL);
 
 app.set('view engine', 'ejs');
 
